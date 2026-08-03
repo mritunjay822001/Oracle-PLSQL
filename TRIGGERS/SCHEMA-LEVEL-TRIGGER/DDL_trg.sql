cf3cc715c4205f3afccb85c9c945775465755cbd
MRITUNJAY KUMAR <mritunjay1333@gmail.com>
2026-08-03 20:50:40 +0530

    Fix formatting and comments in ddl_trg.sql

--- TRIGGERS/SCHEMA-LEVEL-TRIGGER/DDL_trg.sql
+++ TRIGGERS/SCHEMA-LEVEL-TRIGGER/DDL_trg.sql
@@ -1,5 +1,5 @@
 --define a trigger to restrict user[c##mritunjay] toi perform ddl operations
---DDL(alter,flashback,purge,truncate,rename)
+--DDL(alter,purge,truncate,rename)
  CREATE OR REPLACE TRIGGER ddl_trg
   before ALTER OR  purge OR TRUNCATE  OR rename
    ON c##mritunjay.SCHEMA 
